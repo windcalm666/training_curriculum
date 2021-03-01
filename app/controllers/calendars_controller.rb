a71514662dd60a381f6098332a5422387014e851
windcalm666 <79242740+windcalm666@users.noreply.github.com>
2021-03-01 15:39:07 +0900

    plan_paramsメソッドより:wdayの削除修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,7 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:plan).permit(:date, :plan, :wday)
+    params.require(:plan).permit(:date, :plan)
   end
 
   def get_week
